bf8a4d4eef059604f68ecff6ea025efd90abb6c7
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-11-11 10:34:39 +0900

    Add a test for ShoppingCart class

--- recipe11.py
+++ recipe11.py
@@ -10,7 +10,7 @@ class ShoppingCart(object):
         return self
 
     def item(self, index):
-        return self.items[index-1].price
+        return self.items[index-1].item
 
     def price(self, index):
         return self.items[index-1].price
@@ -26,3 +26,22 @@ class Item(object):
     def __init__(self, item, price):
         self.item = item
         self.price = price
+
+import unittest
+class ShoppingCartTest(unittest.TestCase):
+    def setUp(self):
+        self.cart = ShoppingCart().add('tuna sandwich', 15.00)
+
+    def test_length(self):
+        self.assertEquals(1, len(self.cart))
+
+    def test_item(self):
+        self.assertEquals('tuna sandwich', self.cart.item(1))
+
+    def test_price(self):
+        self.assertEquals(15.00, self.cart.price(1))
+
+    def test_total_with_sales_tax(self):
+        self.assertAlmostEquals(16.39, \
+                                    self.cart.total(9.25), 2)
+
