ac201d754128fedb50a8435dd170bcf46b7ac37f
tmaeda1981jp <tmaeda1981jp@gmail.com>
2012-11-12 01:55:21 +0900

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+try:
+    import ez_setup
+    ez_setup.use_setuptools()
+except ImportError:
+    pass
+
+from setuptools import setup
+
+setup(
+    name="RegexPicker plugin",
+    version="0.1",
+    author="tmaeda1981jp",
+    author_email="tmaeda1981jp at gmail",
+    description="Pick test methods based on a regular expression",
+    license="Apache Server Licence 2.0",
+    py_modules=["recipe13_plugin"],
+    entry_points = {
+        'nose.plugins': [
+            'recipe13_plugin = recipe13_plugin:RegexPicker'
+        ]
+    }
+)
